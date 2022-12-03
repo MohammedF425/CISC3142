407a62c9332542532b8add8807ba96c1dc5aa7eb
Mohammed Fadel <fadel@Mohammeds-MacBook-Pro.local>
2022-12-03 07:58:56 -0500

    uploading

--- lab10/exer10.cpp
+++ lab10/exer10.cpp
@@ -6,27 +6,56 @@
 #include <cstdlib>
 #include <numeric>
 
-template <class T>
-class Comparator { // we pass an object of this class as
-                   // third arg to sort function...
-public:
-    bool operator()(T s1, T s2)
-    {
-            string temp=s1.substr(s1.find(';')+1);
-        for(int j=0;j<6;j++)
-            temp = temp.substr(temp.find(';')+1);
-        temp = temp.substr(0,temp.find(';'));
-    string temp2=s2.substr(s2.find(';')+1);
-        for(int j=0;j<6;j++)
-            temp2 = temp2.substr(temp2.find(';')+1);
-        temp = temp2.substr(0,temp2.find(';'));
-        return stoi(temp) < stoi(temp2);
-    }
-};
 
 using namespace std;
 
-vector<string> readData(){
+class Movie{
+    public:
+    int year;
+    int len;
+    string title;
+    string subject;
+    string actor;
+    string actress;
+    string director;
+    int popularity;
+    string Awards;
+    string image;
+    // Movie(int a,int b, string c,string d,string e,string f,string g,int h,string i,string j ){
+    //  year=a;
+    //  len=b;
+    //  title=c;
+    //  subject=d;
+    //  actor=e;
+    //  actress=f;
+    //  director=g;
+    //  popularity=h;
+    //  Awards=i;
+    //  image=j;
+    // }
+
+    bool operator < (const Movie& movie) const
+    {
+        return (popularity<movie.popularity);
+    }
+    // bool operator==(const Movie& movie) const 
+    // { 
+    //     return (title == movie.title);
+    // }
+
+
+};
+
+string getLine(string data,ifstream& infile, char ch){
+    getline(infile, data, ch);
+    if(data==""){
+        data ='0';
+    }
+    return data;
+
+}
+
+vector<Movie> readData(){
     ifstream inFile;
     inFile.open("inputFile.txt");
 
@@ -34,92 +63,60 @@ vector<string> readData(){
         cout<<"Cant open file error"<<endl;
         exit(EXIT_FAILURE);
     }
-    vector<string> input;    
+    vector<Movie> input;    
     string data;
     char infile[150]="inputFile.txt";
     inFile.getline(infile,150);
     inFile.getline(infile,150);
+    int i=0;
+    while(getline(inFile,data,';')){
+        Movie temp;
+        
+        i++;
+        temp.year = stoi(data);
+        temp.len=stoi(getLine(data,inFile,';'));
+        temp.title=getLine(data,inFile,';');
+        temp.subject=getLine(data,inFile,';');
+        temp.actor=getLine(data,inFile,';');
+        temp.actress=getLine(data,inFile,';');
+        temp.director=getLine(data,inFile,';');
+        temp.popularity=stoi(getLine(data,inFile,';'));
+        temp.Awards=getLine(data,inFile,';');
 
-    while(getline(inFile,data)){
-        input.push_back(data);
+        temp.image=getLine(data,inFile,'.')+".png";
+        getline(inFile,data);
+        
+        input.push_back(temp);
     }
     return input;
 
 }
 
-int totalTime(vector<string> vec){
-    vector<int> len;
-    for(int i =0; i<vec.size();i++){
-        string num = vec[i].substr(vec[i].find(';') +1);
-        num = num.substr(0,num.find(';'));
-        if(num.length()==0)
-            num = '0';
-        len.push_back(stoi(num));
-        // cout<<num<<" "<< i<<endl;
-
-    }
-    int sum = accumulate(len.begin(),len.end(),0);
-    return sum;
-}
-// bool myCmp(string s1, string s2)
-// {
-    string temp=s1.substr(s1.find(';')+1);
-        for(int j=0;j<6;j++)
-            temp = temp.substr(temp.find(';')+1);
-        temp = temp.substr(0,temp.find(';'));
-    string temp2=s2.substr(s2.find(';')+1);
-        for(int j=0;j<6;j++)
-            temp2 = temp2.substr(temp2.find(';')+1);
-        temp = temp2.substr(0,temp2.find(';'));
-
-    
-//         s1=s1.substr(s1.find(';')+1);
-//         for(int j=0;j<6;j++)
-//             s1 = s1.substr(s1.find(';')+1);
-//         s1 = s1.substr(0,s1.find(';'));
-
-//         s2=s2.substr(s2.find(';')+1);
-//         for(int j=0;j<6;j++)
-//             s2 = s2.substr(s2.find(';')+1);
-//         s2 = s2.substr(0,s2.find(';'));
-//      if (s1. == s2.size()) {
-//         return s1 < s2;
-//     }
-
-//     else {
-//         return s1.size() < s2.size();
-//     }
+// void findMovie(string title, vector<Movie> movies){
+//     // find(movies.begin(),movies.end(),title);
 // }
 
-vector<string> sortMovies(vector<string> vec){
-    vector<string> movies;
-    for(int i=0;i<vec.size();i++){
-        string temp=vec[i].substr(vec[i].find(';')+1);
-        for(int j=0;j<6;j++)
-            temp = temp.substr(temp.find(';')+1);
-        temp = temp.substr(0,temp.find(';'));
-        string temp2 = vec[i].substr(vec[i].find(';')+1);
-        temp2 = temp2.substr(temp2.find(';')+1);
-        temp2 = temp2.substr(0,temp2.find(';'));
-        temp += ": "+temp2;
-        movies.push_back(temp);
-        cout<<movies[i]<<endl;
-    }
-     sort(movies.begin(),movies.end());
-     for(int i =0; i<10;i++)
-        cout<<movies[i]<<endl;
 
-    return vec;
-}
+
+
 int main(){
-    vector<string> vec = readData();
-    //int sum = totalTime(vec);
-    // cout << sum<<endl;
+    vector<Movie> movies = readData();
+    sort(movies.begin(), movies.end());
+    for(int i=0;i<20;i++){
+        cout<<movies[i].popularity<<endl;
+    }
 
-    sortMovies(vec);
-    sort(vec.begin(),vec.end(),Comparator<int>())
+    int sum;
+    vector<int>temp;
+    for(int i=0;i<movies.size();i++){
+        temp.push_back(movies[i].year);
+    }
+    sum=accumulate(temp.begin(),temp.end(),0);
+    cout<<sum<<endl;
+    
 
+    //find(movies.begin(),movies.end(),title);
+
+    // COULDN'T FINISH FIND OR UNIQUE AS I WAS HAVING TROUBLE WITH THE OPERATOR 
+    // OVERLOAD PART
 }
-
-
-
